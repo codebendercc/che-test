756ed73dd47e57adfa847343ee8538119a954eef
Vasilis Georgitzikis <tzikis@gmail.com>
2016-04-17 03:26:07 +0000

    moved build files to a build folder

--- compile-blink.sh
+++
@@ -1,4 +0,0 @@
-#/bin/bash
-
-./arduino-builder-linux -compile -logger=machine -hardware ./hardware-linux -tools ./tools-builder-linux -tools ./hardware-linux/tools/avr -built-in-libraries ./libraries-linux -fqbn=esp8266com:esp8266:huzzah:CpuFrequency=80,UploadSpeed=115200,FlashSize=4M3M -ide-version=10608 -build-path "/tmp/cbtest" -warnings=none -prefs=build.warn_data_percentage=75 -verbose Blink/Blink.ino &&\
-cp /tmp/cbtest/Blink.ino.bin ./binaries/
