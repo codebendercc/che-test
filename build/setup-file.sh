02bc88b90187c2475daec712abc0558414324c50
Vasilis Georgitzikis <tzikis@gmail.com>
2016-04-18 00:47:00 +0000

    Incr version only on successful compile

--- build/setup-file.sh
+++ build/setup-file.sh
@@ -2,22 +2,18 @@
 
 tempdir=$2
 
-echo "Generating $tempdir"
-mkdir -p $tempdir
-
 filename="$1.ino"
 filepath="sketches/$1/$filename"
 temppath="$tempdir/$filename"
 
 version=$(cat /tmp/version)
 echo "Old version: $version"
-
 ((version++))
-
 echo "New version: $version"
-echo $version > /tmp/version$version > /tmp/version
 
-echo "Configuring $filepath with new version on $temppath"
-sed -e "s/__CODEBENDER_AUTO_VERSION__/$version/g" -e "s/__CODEBENDER_UPDATE_URL__/http:\/\/che-test-tzikis1.c9users.io\/test.php/g" $filepath > $temppath
-
+echo "Generating $tempdir"
+mkdir -p $tempdir && \
+\
+echo "Configuring $filepath with new version on $temppath" && \
+sed -e "s/__CODEBENDER_AUTO_VERSION__/$version/g" -e "s/__CODEBENDER_UPDATE_URL__/http:\/\/che-test-tzikis1.c9users.io\/test.php/g" $filepath > $temppath && \
 echo "New file at $temppath"
