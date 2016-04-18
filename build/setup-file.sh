52a0fae79fc56120afd9fa90cd66c333134e526a
Vasilis Georgitzikis <tzikis@gmail.com>
2016-04-18 07:40:34 +0000

    renamed update script to update.php

--- build/setup-file.sh
+++ build/setup-file.sh
@@ -15,5 +15,5 @@ echo "Generating $tempdir"
 mkdir -p $tempdir && \
 \
 echo "Configuring $filepath with new version on $temppath" && \
-sed -e "s/__CODEBENDER_AUTO_VERSION__/$version/g" -e "s/__CODEBENDER_UPDATE_URL__/http:\/\/che-test-tzikis1.c9users.io\/test.php/g" $filepath > $temppath && \
+sed -e "s/__CODEBENDER_AUTO_VERSION__/$version/g" -e "s/__CODEBENDER_UPDATE_URL__/http:\/\/che-test-tzikis1.c9users.io\/update.php/g" $filepath > $temppath && \
 echo "New file at $temppath"
