7a736b4d40aba6faf93831e710750321ee02fc74
Vasilis Georgitzikis <tzikis@gmail.com>
2016-04-19 00:36:42 +0000

    Added support for providing fqbn, default is Adafruit Feather

--- build/compile.sh
+++ build/compile.sh
@@ -5,11 +5,13 @@ tempdir="/tmp/$randfolder"
 # sketch=$1
 sketchname="sketch"
 
+if [ -n "$1" ]; then fqbn=$1; else fqbn="esp8266com:esp8266:huzzah:CpuFrequency=80,UploadSpeed=115200,FlashSize=4M3M"; fi
+
 ./build/setup-file.sh $sketchname $tempdir &&\
 
 cd build &&\
 mkdir -p /tmp/cbtest &&\
-./arduino-builder-linux -compile -logger=machine -hardware ./hardware-linux -tools ./tools-builder-linux -tools ./hardware-linux/tools/avr -built-in-libraries ./libraries-linux -fqbn=esp8266com:esp8266:huzzah:CpuFrequency=80,UploadSpeed=115200,FlashSize=4M3M -ide-version=10608 -build-path "/tmp/cbtest" -warnings=none -prefs=build.warn_data_percentage=75 -verbose $tempdir/$sketchname.ino &&\
+./arduino-builder-linux -compile -logger=machine -hardware ./hardware-linux -tools ./tools-builder-linux -tools ./hardware-linux/tools/avr -built-in-libraries ./libraries-linux -fqbn=$fqbn -ide-version=10608 -build-path "/tmp/cbtest" -warnings=none -prefs=build.warn_data_percentage=75 -verbose $tempdir/$sketchname.ino &&\
 ./bump-version.sh && \
 version=$(cat /tmp/version) && \
 cp /tmp/cbtest/$sketchname.ino.bin ../binaries/$sketchname.ino.bin.v$version &&\
